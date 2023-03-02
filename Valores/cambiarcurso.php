081bc0954bcfebd9304ddf08dffed5f64565c349
Daniel Taboada <119310520+dtaboadaS@users.noreply.github.com>
2023-03-02 13:42:14 +0100

    Update cambiarcurso.php

--- Valores/cambiarcurso.php
+++ Valores/cambiarcurso.php
@@ -6,7 +6,7 @@
 	<h1 class="center">Cambio de datos de curso</h1>
 <?php
 // Establecer la conexión con la base de datos
-$conexion = mysqli_connect("localhost", "usuario", "abc123.", "universidad");
+include '../bbdd/conector.php';
 
 // Obtener los datos del formulario
 $Cod_Curso = $_POST['Cod_Curso'];
